e7e3f1d205752a95e0165181800b7d04d8de166f
Dhrubajyoti Biswas <dsofthelp.org@outlook.com>
2023-07-15 22:57:38 +0530

    Add files via upload

---
+++ web-app/web-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import MenuBar from './components/MenuBar'
+import MonacoEditor from './components/Editor/MonacoEditor'
+
+function App() {
+  return (
+    <>
+      <MenuBar />
+      {/* <MonacoEditor /> */}
+      
+    </>
+  )
+}
+
+export default App
