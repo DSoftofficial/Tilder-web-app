78ef9f483dece7d78b32fb4870633734820781a0
Dhrubajyoti Biswas <dsofthelp.org@outlook.com>
2023-07-16 20:18:06 +0530

    Add files via upload

--- web-app/web-app/src/App.jsx
+++ web-app/web-app/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import MenuBar from './components/MenuBar'
+import MenuBar from './components/MenuBar/MenuBar'
 import MonacoEditor from './components/Editor/MonacoEditor'
+import SideBar from './components/SideBar/SideBar'
+
 
 function App() {
   return (
     <>
       <MenuBar />
-      {/* <MonacoEditor /> */}
-      
+      <MonacoEditor />
+      <SideBar />
     </>
   )
 }
---
+++ web-app/web-app/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <>
+    <div className="sidebar">
+        
+    </div>
+    </>
+  )
+}
