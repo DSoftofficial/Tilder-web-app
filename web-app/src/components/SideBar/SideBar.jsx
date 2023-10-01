a236064d27b6308c952e4d4d352d541d6650a2c7
Dhrubajyoti Biswas <dsofthelp.org@outlook.com>
2023-10-01 15:11:37 +0530

    Add files via upload

--- web-app/src/components/SideBar/SideBar.jsx
+++ web-app/src/components/SideBar/SideBar.jsx
@@ -5,14 +5,12 @@ export default function SideBar() {
     <>
     <div className="sidebar">
         <div className="fileexplorer sidebaricons"><i className="fa-regular fa-file fa-xl"></i></div>
-        <div className="fileexplorer sidebaricons"><i className="fa-solid fa-magnifying-glass fa-xl"></i></div>
-        <div className="fileexplorer sidebaricons"><i className="fa-solid fa-terminal fa-xl"></i></div>
-        <div className="fileexplorer sidebaricons"><i className="fa-solid fa-bug-slash fa-xl"></i></div>
-        <div className="fileexplorer sidebaricons"><i className="fa-brands fa-git-alt fa-xl"></i></div>
-        <div className="fileexplorer sidebaricons"><i className="fa-brands fa-github fa-xl"></i></div>
-        <div className="fileexplorer sidebaricons"><i className="fa-solid fa-puzzle-piece fa-xl"></i></div>
-        <div className="fileexplorer sidebaricons"><i className="fa-regular fa-file fa-xl"></i></div>
-        <div className="fileexplorer sidebaricons"><i className="fa-regular fa-file fa-xl"></i></div>
+        <div className="search sidebaricons"><i className="fa-solid fa-magnifying-glass fa-xl"></i></div>
+        <div className="terminal sidebaricons"><i className="fa-solid fa-terminal fa-xl"></i></div>
+        <div className="debug sidebaricons"><i className="fa-solid fa-bug-slash fa-xl"></i></div>
+        <div className="git sidebaricons"><i className="fa-brands fa-git-alt fa-xl"></i></div>
+        <div className="github sidebaricons"><i className="fa-brands fa-github fa-xl"></i></div>
+        <div className="extensions sidebaricons"><i className="fa-solid fa-puzzle-piece fa-xl"></i></div>
     </div>
     </>
   )
