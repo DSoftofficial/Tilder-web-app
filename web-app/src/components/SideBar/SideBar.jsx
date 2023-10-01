c56e188923ded800a8fa8f0c2ce8d83f27b631a1
Dhrubajyoti Biswas <dsofthelp.org@outlook.com>
2023-10-01 15:09:50 +0530

    Add files via upload

--- web-app/src/components/SideBar/SideBar.jsx
+++ web-app/src/components/SideBar/SideBar.jsx
@@ -4,7 +4,15 @@ export default function SideBar() {
   return (
     <>
     <div className="sidebar">
-        
+        <div className="fileexplorer sidebaricons"><i className="fa-regular fa-file fa-xl"></i></div>
+        <div className="fileexplorer sidebaricons"><i className="fa-solid fa-magnifying-glass fa-xl"></i></div>
+        <div className="fileexplorer sidebaricons"><i className="fa-solid fa-terminal fa-xl"></i></div>
+        <div className="fileexplorer sidebaricons"><i className="fa-solid fa-bug-slash fa-xl"></i></div>
+        <div className="fileexplorer sidebaricons"><i className="fa-brands fa-git-alt fa-xl"></i></div>
+        <div className="fileexplorer sidebaricons"><i className="fa-brands fa-github fa-xl"></i></div>
+        <div className="fileexplorer sidebaricons"><i className="fa-solid fa-puzzle-piece fa-xl"></i></div>
+        <div className="fileexplorer sidebaricons"><i className="fa-regular fa-file fa-xl"></i></div>
+        <div className="fileexplorer sidebaricons"><i className="fa-regular fa-file fa-xl"></i></div>
     </div>
     </>
   )
